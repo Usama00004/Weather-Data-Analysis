ff610630526a7d093096101c0df036b5a5f5a750
Usama Tahir <usamatahir00004@gmail.com>
2025-01-26 22:04:38 +0100

    Fetching data..

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from scripts.fetch_data import fetch_weather_data
 
+
 def main():
     city = "Berlin"
     country = "DE"
@@ -7,10 +8,6 @@ def main():
     print("Fetching weather data...")
     # weather_data = fetch_weather_data(city, country)
     fetch_weather_data()
-    # if weather_data:
-    #     print("Weather Data:", weather_data)
-    # else:
-    #     print("Failed to fetch weather data.")
-
+   
 if __name__ == "__main__":
     main()
--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -1,27 +1,44 @@
 import requests
-from scripts.config import API_KEY, BASE_URL,URL
+from scripts.config import URL
+import pandas as pd
+import os
+
 
 
 def fetch_weather_data():
-    """Fetch weather data from the API for a specific city and country."""
-    # url = f"{BASE_URL}/weather?q={city},{country}&appid={API_KEY}"
-    
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json()  
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
-        return None
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'country-coord.csv')
+    df_coordinates = pd.read_csv(os.path.abspath(file_path))
 
+    # Get the latitude and longitude columns
+    latitudes = df_coordinates['Latitude']
+    longitudes = df_coordinates['Longitude']
+    country = df_coordinates['Country']
 
-# def fetch_weather_data(city, country):
-#     """Fetch weather data from the API for a specific city and country."""
-#     url = f"{BASE_URL}/weather?q={city},{country}&appid={API_KEY}"
-#     try:
-#         response = requests.get(url)
-#         response.raise_for_status()
-#         return response.json()  
-#     except requests.exceptions.RequestException as e:
-#         print(f"Error fetching data: {e}")
-#         return None
+    # Base URL for Open-Meteo API
+    url = "https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true"
+    weather_data_list = []
+    # Loop through the latitudes and longitudes and call the API for each pair
+    for latitude, longitude in zip(latitudes, longitudes):
+        formatted_url = url.format(latitude=latitude, longitude=longitude)
+        response = requests.get(formatted_url)
+        if response.status_code == 200:
+            # Process the response if the request is successful
+            data = response.json()
+            current_weather = data.get('current_weather', {})
+            if current_weather:
+                weather_data = {  
+                    'latitude': latitude,
+                    'longitude': longitude,
+                    'time': current_weather.get('time'),
+                    'temperature': current_weather.get('temperature'),
+                    'windspeed': current_weather.get('windspeed'),
+                    'winddirection': current_weather.get('winddirection')
+                }
+                weather_data_list.append(weather_data)
+            else:
+                print("Failed to fetch weather_data ")    
+             
+        else:
+            print(f"Failed to retrieve data for {latitude}, {longitude}. Status code: {response.status_code}")
+        
+        print(len(weather_data_list))
