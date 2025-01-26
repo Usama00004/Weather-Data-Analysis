ff4deae0fa08c4dcf29fef7a58e031b14c3742b2
Usama Tahir <usamatahir00004@gmail.com>
2025-01-26 22:44:53 +0100

    Completing transforming_data.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from scripts.fetch_data import fetch_weather_data
+from scripts.process_data import process_weather_data
 
 
 def main():
 
     print("Fetching weather data...")
     fetch_weather_data()
+
+    print("transforming weather data...")   
+    process_weather_data()
    
 if __name__ == "__main__":
     main()
--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,9 +1,18 @@
 import os
 import pandas as pd
 
-
-def data_processor():
+def process_weather_data():
     file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'raw_weather_data.csv')
     df_to_transform = pd.read_csv(os.path.abspath(file_path))
-
-
+    
+    # Apply transformations to 'time' and 'date' columns
+    df_to_transform['time'] = df_to_transform['time'].apply(lambda x: x.split('T')[1])
+    df_to_transform['date'] = df_to_transform['date'].apply(lambda x: x.split('T')[0])
+    
+    # Specify the path for the new CSV file
+    output_file = os.path.join(os.path.dirname(__file__), '..', 'data', 'transformed_weather_data.csv')
+    
+    # Write the transformed DataFrame to a new CSV file
+    df_to_transform.to_csv(output_file, index=False)
+    
+    print(f"Transformed data has been written to '{output_file}'.")
